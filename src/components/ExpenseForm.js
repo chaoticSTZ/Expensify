383737df9daba743ad1267a9e3382eb16eff77f3
Joe <32425596+chaoticSTZ@users.noreply.github.com>
2018-02-13 10:57:26 -0600

    fixed regex in onAmountChange

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -25,10 +25,11 @@ export default class ExpenseForm extends React.Component {
 	onAmountChange = (e) => {
 		const amount = e.target.value;
 		//Regex to match numbers AND decimal with limit to 2 characters after decimal
-		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+		if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
 			this.setState(() => ({ amount }));
 		}
 	};
+	// Single Date Picker (calendar) Functions
 	onDateChange = (createdAt) => {
 		this.setState(() => ({ createdAt }));
 	};
