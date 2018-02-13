efa9b21c0c79d63d9a0b4c88924ad7e0b3f85248
Joe <32425596+chaoticSTZ@users.noreply.github.com>
2018-02-13 12:08:25 -0600

    edit expensepage finished

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 	<div>
 		<Link to={`/edit/${id}`}>
 			<h3>{description}</h3>
@@ -11,21 +9,7 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
 		<p>
 			{amount} - {createdAt}
 		</p>
-		<button
-			onClick={() => {
-				dispatch(removeExpense({ id }));
-			}}
-		>
-			Remove
-		</button>
 	</div>
 );
 
-const mapStateToProps = (state) => {
-	return {
-		expenses: state.expenses,
-		filters: state.filters
-	};
-};
-
-export default connect(mapStateToProps)(ExpenseListItem);
+export default ExpenseListItem;
