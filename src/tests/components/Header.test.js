07ac7961a5ce11456f708e80fd6821318013fe5c
Joe <32425596+chaoticSTZ@users.noreply.github.com>
2018-02-14 14:03:45 -0600

    added more testing

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,9 +5,4 @@ import Header from '../../components/Header';
 test('should render header correctly', () => {
 	const wrapper = shallow(<Header />);
 	expect(wrapper).toMatchSnapshot();
-
-	// expect(wrapper.find('h1').length).toBe(1);
-	// const renderer = new ReactShallowRenderer();
-	// renderer.render(<Header />);
-	// expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
