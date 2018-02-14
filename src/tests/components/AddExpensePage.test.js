41bd468489d0f76a8adc53c69ebb0a6a490460bb
Joe <32425596+chaoticSTZ@users.noreply.github.com>
2018-02-14 16:25:18 -0600

    tweeked AddExpensePage to a class component

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddExpensePage } from '../../components/AddExpensePage';
+
+test('should render AddExpensePage correctly', () => {});
