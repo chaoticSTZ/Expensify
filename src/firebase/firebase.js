89f6c1061307a6da4657140e22a17eb2ffae7e87
Joe <32425596+chaoticSTZ@users.noreply.github.com>
2018-03-06 11:58:39 -0600

    firebase stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,38 +13,84 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database
-	.ref()
-	.set({
-		name: 'Joe Gallegos',
-		age: 29,
-		isSingle: true,
-		location: {
-			city: 'Dallas',
-			country: 'United States'
-		}
-	})
-	.then(() => {
-		console.log('data is saved');
-	})
-	.catch((e) => {
-		console.log('This failed', e);
-	});
+database.ref('expenses').push({
+	description: 'Lunch',
+	note: '',
+	amount: '4500',
+	createdAt: 'ksjdahbfksdf'
+});
 
-// database.ref().set('this is my data');
+database.ref('expenses').push({
+	description: 'Rent',
+	note: 'March rent',
+	amount: '$65000',
+	createdAt: 'asdftrgh'
+});
 
-// database.ref('age').set(30);
-// database.ref('location/city').set('Denver');
+database.ref('expenses').push({
+	description: 'gas',
+	note: 'small amount of gas',
+	amount: '$2500',
+	createdAt: 'ergfntyj'
+});
 
-database
-	.ref('attributes')
-	.set({
-		height: 69,
-		weight: 190
-	})
-	.then(() => {
-		console.log('the data is saved');
-	})
-	.catch((e) => {
-		console.log('There was an error', e);
-	});
+// database.ref('notes/-L6wPCn6-iASHHWnIdHm').remove();
+
+// database.ref('notes').push({
+// 	title: 'Course Topics',
+// 	body: 'React Native, Angular'
+// });
+
+// database.ref().on('value', (snapshot) => {
+// 	const val = snapshot.val();
+// 	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
+
+// database
+// 	.ref('location/city')
+// 	.once('value')
+// 	.then((snapshot) => {
+// 		const val = snapshot.val();
+// 		console.log(val);
+// 	})
+// 	.catch((e) => {
+// 		console.log('Error fetching data', e);
+// 	});
+
+// database
+// 	.ref()
+// 	.set({
+// 		name: 'Joe Gallegos',
+// 		age: 29,
+// 		stressLevel: 6,
+// 		job: {
+// 			title: 'software developer',
+// 			company: 'Google'
+// 		},
+// 		location: {
+// 			city: 'Dallas',
+// 			country: 'United States'
+// 		}
+// 	})
+// 	.then(() => {
+// 		console.log('data is saved');
+// 	})
+// 	.catch((e) => {
+// 		console.log('This failed', e);
+// 	});
+
+// database.ref().update({
+// 	stressLevel: 9,
+// 	'job/company': 'Amazon',
+// 	'location/city': 'Seattle'
+// });
+
+// database
+// 	.ref()
+// 	.remove()
+// 	.then(() => {
+// 		console.log('data was removed');
+// 	})
+// 	.catch((e) => {
+// 		console.log('data not removed', e);
+// 	});
